425ef979c12a1d83457004ff8a02f58f35e64da1
Samuel Berto <samuzberto1k@gmail.com>
2026-08-25 23:42:25 -0300

    Implementa atualização de produto via SQL (UPDATE)

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class Main {
     public static void main(String[] args) {
         ConexaoBanco.criarTabela();
@@ -17,7 +16,7 @@ public class Main {
             System.out.println("\n=== Controle de Estoque ===");
             System.out.println("1 - Cadastrar produto");
             System.out.println("2 - Listar produtos");
-            System.out.println("3 - Atualizar quantidade");
+            System.out.println("3 - Atualizar produto (quantidade e preço)");
             System.out.println("4 - Calcular valor total do estoque");
             System.out.println("0 - Sair");
             System.out.print("Escolha uma opção: ");
@@ -37,22 +36,34 @@ public class Main {
 
                     Produto novoProduto = new Produto(nome, quantidade, preco);
                     ProdutoDAO.cadastrar(novoProduto);
-                
+
                 }
                 case 2 -> {
                     System.out.println("\n--- Produtos cadastradors ---");
                     ArrayList<Produto> produtosDoBanco = ProdutoDAO.listar();
                     for (Produto p : produtosDoBanco) {
-                        System.out.println(p.getNome() + " | Qtd: " + p.getQuantidade() + " | Preço: R$" + p.getPreco());
+                        System.out
+                                .println(p.getNome() + " | Qtd: " + p.getQuantidade() + " | Preço: R$" + p.getPreco());
                     }
                 }
-                case 3 -> System.out.println("Você escolheu atualizar quantidade.");
+                case 3 -> {
+                    System.out.print("Nome do produto a atualizar: ");
+                    String nomeProduto = scanner.nextLine();
+                    System.out.print("Nova quantidade: ");
+                    int novaQuantidade = scanner.nextInt();
+                    System.out.print("Novo preço: ");
+                    double novoPreco = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    ProdutoDAO.atualizarProduto(nomeProduto, novaQuantidade, novoPreco);
+                }
                 case 4 -> {
                     System.out.println("\n--- Valor total por Produto ---");
                     double total = 0;
                     for (Produto p : estoque) {
                         double valorProduto = p.getValorTotal();
-                        System.out.println(p.getNome () + " : " + p.getQuantidade() + " x R$" + p.getPreco () + " = R$" + valorProduto);
+                        System.out.println(p.getNome() + " : " + p.getQuantidade() + " x R$" + p.getPreco() + " = R$"
+                                + valorProduto);
                         total += valorProduto;
                     }
                     System.out.println("\n Valor Total do Estoque: R$" + total);
--- ProdutoDAO.java
+++ ProdutoDAO.java
@@ -7,7 +7,7 @@ public class ProdutoDAO {
         String sql = "INSERT INTO produtos (nome, quantidade, preco) VALUES (?, ?, ?)";
 
         try (Connection conexao = ConexaoBanco.conectar();
-            PreparedStatement stmt = conexao.prepareStatement(sql)) {
+                PreparedStatement stmt = conexao.prepareStatement(sql)) {
 
             stmt.setString(1, produto.getNome());
             stmt.setInt(2, produto.getQuantidade());
@@ -27,9 +27,9 @@ public class ProdutoDAO {
         String sql = "SELECT * FROM produtos";
 
         try (Connection conexao = ConexaoBanco.conectar();
-             PreparedStatement stmt = conexao.prepareStatement(sql);
-             java.sql.ResultSet rs = stmt.executeQuery()) {
-                
+                PreparedStatement stmt = conexao.prepareStatement(sql);
+                java.sql.ResultSet rs = stmt.executeQuery()) {
+
             while (rs.next()) {
                 String nome = rs.getString("nome");
                 int quantidade = rs.getInt("quantidade");
@@ -43,4 +43,28 @@ public class ProdutoDAO {
 
         return produtos;
     }
+
+    public static void atualizarProduto(String nome, int novaQuantidade, double novoPreco) {
+        String sql = "UPDATE produtos SET quantidade =?, preco = ? WHERE nome = ?";
+
+        try (Connection conexao = ConexaoBanco.conectar();
+                PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+            stmt.setInt(1, novaQuantidade);
+            stmt.setDouble(2, novoPreco);
+            stmt.setString(3, nome);
+
+            int linhasAfetadas = stmt.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Produto atualizado com sucesso!");
+            } else {
+                System.out.println("Produto não encontrado.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error ao atualizar produto: " + e.getMessage());
+        }
+    }
+
 }
